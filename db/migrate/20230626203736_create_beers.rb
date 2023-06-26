b567bddaea040df54a8dadbc39d49302f1e0fa07
Edouard de Montchenu <edouarddemontchenu@gmail.com>
2023-06-26 22:38:38 +0200

    beer model

---
+++ db/migrate/20230626203736_create_beers.rb
@@ -0,0 +1,13 @@
+class CreateBeers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.string :country
+      t.string :type
+      t.integer :abv
+      t.string :poster_url
+
+      t.timestamps
+    end
+  end
+end
