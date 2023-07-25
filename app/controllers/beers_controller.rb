721db8eafcaf8f53cce958fac18d812fd39c1474
Edouard de Montchenu <edouarddemontchenu@gmail.com>
2023-07-25 16:14:43 +0200

    added controller

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -1,2 +1,5 @@
 class BeersController < ApplicationController
+  def index
+    @beers = Beer.all
+  end
 end
--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,10 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new
+    @
+  end
 end
